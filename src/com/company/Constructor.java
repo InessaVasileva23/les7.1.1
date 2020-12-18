e711f5de04220c334f5dc96e75a945ab21431320
Inessa <i.vasileva.stc@innopolis.university.ru>
2020-12-18 21:32:28 +0300

    ДЗ

--- src/com/company/Constructor.java
+++ src/com/company/Constructor.java
@@ -1,5 +1,25 @@
 package com.company;
 
-public class Constructor {
+//Реализовать класс в конструкторе которого будет счетчик количества создаваемых объектов.
+// Написать метод для получения информации о количестве.
+
+//класс
+class Constructor {
+    int a;
+    int b;
+    static int NumberofObjects =0;
+
+//конструктор
+    Constructor (){
+        a=1;
+        b=2;
+        NumberofObjects++;
+
+    }
+
+    //метод
+    public int GetNumberofObjects (){
+        return (NumberofObjects);
+    }
 
 }
--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -3,6 +3,7 @@ package com.company;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+        Constructor Constructor = new Constructor();
+        System.out.println("Количество обьектов класса: " +Constructor.GetNumberofObjects());
     }
 }
